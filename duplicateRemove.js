eac267c8b6e2b6061bb38b85b63c0133cd47bc06
Horroon <haroon.rasheed@emumba.com>
2021-09-10 17:46:48 +0500

    optimised removeDuplicate file

--- duplicateRemove.js
+++ duplicateRemove.js
@@ -1,12 +1,14 @@
 function removeDuplicate(numberarry) {
   //Hint: // input = [1,2,2,3,3,2,3,4]
-  const unique = [];
+  //const unique = [];
+  const occurrence = {};
   for (let i = 0; i < numberarry.length; i++) {
-    if (!unique.includes(numberarry[i])) {
-      unique.push(numberarry[i]);
-    }
+    // if (!unique.includes(numberarry[i])) {
+    //   unique.push(numberarry[i]);
+    // }
+    occurrence[numberarry[i]] = i;
   }
-  return unique;
+  return Object.keys(occurrence);
 }
 
 module.exports = removeDuplicate;
--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 // const bubbleSort = require("./bubbleSort");
 // const { countchars, maxchar } = require("./countingSort");
-// const removeDuplicate = require("./duplicateRemove");
+const removeDuplicate = require("./duplicateRemove");
 // const countDisimilarChars = require("./countdisimilarchars");
 // const fabonacciSeries = require("./fabonacci");
 // const findFactorial = require("./factorial");
 // const sameStrings = require("./bothStringEqual");
-const findRepeators = require("./repeatorsarry");
+//const findRepeators = require("./repeatorsarry");
 
-console.log(
-  findRepeators([
-    1, 0, 100, 10, 5, -1, 100, 5, 0, 200, 7, 3445, 26, 26, 26, 100, 0, 2, -1,
-    -1, -1, 2, 11,
-  ])
-);
+console.log(removeDuplicate([1, 2, 2, 3, 4, 3, 5, 6, 4, 4]));
